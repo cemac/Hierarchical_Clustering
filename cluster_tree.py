added BIC score and corrected calculation of medoids

cluster_tree.py
@@ -50,7 +50,7 @@ class ClusterNode(Node):
             root=self.get_root()
             variables=root.variables
             data=np.asarray(root.data)
-            ixs=np.concatenate([np.where(root.indxs==i)[0] for i in self.indxs])
+            ixs=self.get_root_data_indxs()
             data=data[ixs,:]
         else:
             variables=self.variables
@@ -66,6 +66,9 @@ class ClusterNode(Node):
     def print(self, ignore_outlier=False):
         if len(self.variables)>0:
             root_variables=self.variables
+        else:
+            root=self.get_root()
+            root_variables=root.variables
         for pre, _, node in RenderTree(self):
             if (ignore_outlier==False or node.name!=OUTLIER_NAME):
                 data_shape=0
@@ -74,14 +77,10 @@ class ClusterNode(Node):
                 is_leaf_str=''
                 if node.is_leaf:
                     is_leaf_str='is leaf,'
-                if len(self.variables)>0:
-                    variables=self.variables
-                else:
-                    variables=root_variables
                 nmed=len(node.medoid)
                 medoid_str=''
                 if nmed>0:
-                    med_strs=['{v}={m:.2f}'.format(v=variables[i],m=node.medoid[i]) for i in range(nmed)]
+                    med_strs=['{v}={m:.2f}'.format(v=root_variables[i],m=node.medoid[i]) for i in range(nmed)]
                     medoid_str='medoids: '+', '.join(med_strs)+','
                 print('%s%s %s %s' % (pre, node.name, is_leaf_str, medoid_str), len(node.indxs), 'indxs, data.shape=', data_shape)
 
@@ -100,17 +99,24 @@ class ClusterNode(Node):
             
         return name_str
 
-    # if the tree has been created with this code the indxs in root should be np.arange(nsampels)
+    # if the tree has been created with this code the indxs in root should be np.arange(nsamples)
     # if the tree was created using Maryna's code with indxs being the valid data indices then we need to find
     # the matching indxs in a node with what is in root
-    def get_data_indxs(self):
+    def get_root_data_indxs(self):
         root=self.get_root()
         if np.any(abs(root.indxs-np.arange(root.data.shape[0]))>0):
             # the root indxs is not just array from 0 to nsamples
             indxs=np.asarray([np.where(root.indxs==ix)[0][0] for ix in self.indxs])
         else:
             indxs=self.indxs
-        return indxs
+        # we sort the indices because if this is an outlier node the indxs will not neccessarily be in order
+        return np.sort(indxs)
+
+    # returns which of the parent indices form this subcluster
+    def get_indxs_into_parent(self):
+        indxs=np.asarray([np.where(self.parent.indxs==ix)[0][0] for ix in self.indxs])
+        # we sort the indices because if this is an outlier node the indxs will not neccessarily be in order
+        return np.sort(indxs)
 
     # find the end nodes (leaves) of the tree
     def leaves(self, leaves, ignore_outlier=False):
@@ -181,11 +187,10 @@ def write_cluster_tree_hdf(root, clust_type, inner_score_type, outer_score_type,
 #    current_node - the current node (ClusterNode) - None if we haven't created root yet
 #    leaves - a list of the current leaves to which we can add this node if it is a leaf
 #    ignore_outlr - if true don't include  the outlr cluster in leaves
-#    verbose - if true do some printing
 # returns:
 #    root
 #-------------------------------------------------------------------
-def read_keys(group, current_node, leaves, ignore_outlr, verbose):
+def read_keys(group, current_node, leaves, ignore_outlr):
     root=None
     keys=group.keys()
     for key in keys:
@@ -217,15 +222,10 @@ def read_keys(group, current_node, leaves, ignore_outlr, verbose):
                 if isinstance(variables[0], np.bytes_):
                     variables2=[var.decode('utf-8') for var in variables]
                     variables=variables2
-            if verbose:
-                parent_full_name=''
-                if current_node!=None:
-                    parent_full_name=current_node.get_full_name()
-                    print('adding', key, 'to', parent_full_name) 
             new_cluster=ClusterNode(key, current_node, indxs, data=data, variables=variables, centroid=centroid, medoid=medoid)
             if current_node==None:
                 root=new_cluster
-            read_keys(this_group, new_cluster, leaves, ignore_outlr, verbose)
+            read_keys(this_group, new_cluster, leaves, ignore_outlr)
         else:
             raise Exception('unexpected group type')
             
@@ -258,9 +258,14 @@ def read_cluster_tree_hdf(filename, ignore_outlr=False, verbose=False):
         raise err
 
     leaves=[] # a list of the leaves (these are the active clusters)
-    root=read_keys(group, None, leaves, ignore_outlr, verbose)
+    root=read_keys(group, None, leaves, ignore_outlr)
+    if verbose:
+        for k in group.attrs.keys():
+            print(k, group.attrs[k])
+        root.print()
     return root, leaves, group.attrs
 
+    
 #-------------------------------------------------------------------
 # The tree holds an indx of the data that are part of this node for each leaf in the tree.
 # This function switches this information round to give a leaf index (ie cluster index) 
@@ -278,6 +283,6 @@ def get_leaf_index_for_input_indx(leaves, root):
     for n in range(len(leaves)):
         if leaves[n].name==OUTLIER_NAME:
             continue
-        root_ix=leaves[n].get_data_indxs()
+        root_ix=leaves[n].get_root_data_indxs()
         leaf_indxs[root_ix]=n
     return leaf_indxs

hierarchical_clustering.py
@@ -1,12 +1,16 @@
+
 """ 
  Code to perform hierarchical spectral or Kmeans clustering on given data
- The data is transformed to PCA before the clustering.
+ The data is normalised at each level of clustering and can be transformed to
+ PCA space before the clustering.
  The code consists of an inner loop that does the clustering for a single leaf
- of the current tree, and an outer node that decides whether to accept that
+ of the current tree, and an outer loop that decides whether to accept that
  clustering and whether to go to the next level in the hierarchy.
  The user can chose which scores to use for both the inner and outer loop from
- Wemmert-Gancarski, CalinskiHarabasz, Silhouette or DaviesBouldin
+ Wemmert-Gancarski, CalinskiHarabasz, Silhouette, DaviesBouldin or BIC
  If a cluster is too sparsely populated it will be added to an outlier node.
+ The code does not try to split clusters that do not contain more than a certain
+ percentage of the points. 
 
  Authors:
  * Written by Julia Crook, Maryna Lukach
@@ -130,8 +134,9 @@ def get_replacement_current_cluster_labels(old_label, model_labels, next_cluster
 # If better we go round the loop again with nclusters incremented
 #
 # inputs:
-#     this_cluster_data - the data[nsamples,nvars] for the current cluster
+#     this_cluster_data - the normalised data[nsamples,nvars] for the current cluster
 #     evr_threshold - explained variance ratio threshold - we use this to determine how many variables to use in the PCA calculation
+#                    if this is 0 we wont put data into pca space but just normalise the data
 #     min_npoints_to_cluster - if a cluster holds less than this number of points we wont try to subcluster
 #     max_npoints_for_outlier - the number of points that defines a subcluster as being an outlier
 #     scorer_type - the type of scorer to use for the inner loop to determine when to stop
@@ -140,7 +145,6 @@ def get_replacement_current_cluster_labels(old_label, model_labels, next_cluster
 #     verbose - if True, print what is going on
 # returns:
 #     prev_labels - the labels defining the new sub clusters of this cluster
-#     X_new - the pca data for this cluster
 #---------------------------------------------------------------------------------
 def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, max_npoints_for_outlier, scorer_type, cluster_name, use_kmeans=False, verbose=False):
 
@@ -150,9 +154,13 @@ def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, ma
     split_is_better=True
     
     # convert data to pca
-    X_std = QuantileTransformer(output_distribution='normal').fit_transform(this_cluster_data)
-    selected_pca, X_new = get_PCA(X_std, evr_threshold, verbose=verbose)
-    
+    if evr_threshold!=0:
+        selected_pca, X_new = get_PCA(this_cluster_data, evr_threshold, verbose=verbose)
+        if selected_pca==1:
+            raise ValueError('get_subclusters(): only one PC chosen, input_data likely to be invalid')
+    else:
+        X_new=this_cluster_data
+        
     active_indices=np.arange(this_cluster_data.shape[0])  # initially all indices for this data
     nactive=this_cluster_data.shape[0]
     new_labels=np.zeros(this_cluster_data.shape[0], int)
@@ -165,7 +173,7 @@ def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, ma
         if verbose:
             print('get_subclusters: number_of_clusters {}'.format(number_of_clusters))
 
-        X_new_filtered = X_new[active_indices, 0:selected_pca] # used to do a copy?
+        X_new_filtered = X_new[active_indices, 0:selected_pca]
         if verbose:
             print('get_subclusters: doing {} clustering of {} with X_new_filtered.shape = {}'.format(clust_type, cluster_name, X_new_filtered.shape))
 
@@ -247,7 +255,7 @@ def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, ma
         print('get_subclusters: no active cluster any more!!!!!')
         pdb.set_trace()
     print('get_subclusters: loop complete')
-    return prev_labels, X_new
+    return prev_labels
 
 #---------------------------------------------------------------------------------
 # perform_hierarchical_clustering
@@ -256,6 +264,7 @@ def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, ma
 #    cluster_data[namples,nvars] - all the data that will be clustered
 #    field_list - list of the names of the radar variables in the cluster_data (this is stored in root of the tree)
 #    evr_threshold - explained variance ratio threshold - we use this to determine how many variables to use in the PCA calculation
+#                    if this is 0 we wont put data into pca space but just normalise the data
 #    min_pc_points_to_cluster - if a cluster holds less than this % x number of samples we wont try to subcluster
 #    max_pc_points_for_outlier - defines the number of points (this % x number of samples) that defines an outlier
 #    scorer_outer - the type of scorer for the outer loop
@@ -266,14 +275,19 @@ def get_subclusters(this_cluster_data, evr_threshold, min_npoints_to_cluster, ma
 #    verbose - if True, print what is going on
 #---------------------------------------------------------------------------------
 def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min_pc_points_to_cluster, max_pc_points_for_outlier, scorer_outer, scorer_inner, output_dir, plot_fname_prefix, use_kmeans=False, verbose=False):
+
+    ix=np.where(cluster_data==np.nan)
+    if len(ix[0])>0:
+        raise ValueError('perform_hierarchical_clustering(): input_data has nans')
         
     nsamples=cluster_data.shape[0]
     min_npoints_to_cluster=int(min_pc_points_to_cluster*nsamples/100)
     max_npoints_for_outlier=int(max_pc_points_for_outlier*nsamples/100)
+    nvars=cluster_data.shape[1]
     if verbose:
         print(nsamples, 'samples, min points to cluster=', min_npoints_to_cluster, 'max points for outlier=',max_npoints_for_outlier)
-    all_labels = np.zeros(nsamples, int)
-    possible_labels=np.copy(all_labels)
+    all_labels = np.zeros(nsamples, int) # this is the labels we have accepted
+    possible_labels=np.copy(all_labels) # this is the labels that we might accept
     # whole dataset is the root cluster
     # create root node of the cluster-tree
     root = ClusterNode('root',None,np.arange(nsamples),data=cluster_data,variables=field_list)
@@ -307,14 +321,18 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
                 # we tried this before so skip
                 continue
             current_cluster_data = cluster_data[cluster.indxs]
+            # calculate normalised data for this cluster
+            qt=QuantileTransformer(output_distribution='normal')
+            current_cluster_std_data = qt.fit_transform(current_cluster_data)
+
             clust_name = cluster.get_full_name() # get the full name so we know where we are in the hierarchy
             if verbose:
                 print('perform_hierarchical_clustering: we work with cluster {} name {} of size {}, next cluster_label= {}'.format(k,clust_name,cluster_sizes[k],next_cluster_label))
 
             # threshold on the minimum number of points to cluster
-            if (current_cluster_data.shape[0]>min_npoints_to_cluster):
+            if (current_cluster_std_data.shape[0]>min_npoints_to_cluster):
 
-                best_labels, current_cluster_pca_data = get_subclusters(current_cluster_data, evr_threshold, min_npoints_to_cluster, max_npoints_for_outlier, scorer_inner, clust_name, use_kmeans=use_kmeans, verbose=verbose)
+                best_labels = get_subclusters(current_cluster_std_data, evr_threshold, min_npoints_to_cluster, max_npoints_for_outlier, scorer_inner, clust_name, use_kmeans=use_kmeans, verbose=verbose)
                 # find outliers
                 ix_outlier=np.where(best_labels==OUTLIER_LABEL)[0]
                 noutliers=len(ix_outlier)
@@ -323,13 +341,14 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
                 nnon_outlier=len(ix_non_outlier)
                 if verbose:
                     print('perform_hierarchical_clustering: ', noutliers, 'outlier points', nnon_outlier, 'non outlier points in cluster', clust_name)
-                # Use ix_non_outlier to filter current_cluster_data, current_cluster_indxs for further processing
+                # Use ix_non_outlier to filter current_cluster_data, and best_labels for further processing
                 current_cluster_data_filtered = current_cluster_data[ix_non_outlier]
-                current_cluster_pca_data_filtered=current_cluster_pca_data[ix_non_outlier]
-                current_cluster_indxs_filtered = cluster.indxs[ix_non_outlier]
-                nsub_clusters_filtered = len(np.unique(best_labels[ix_non_outlier]))
+                current_cluster_std_data_filtered=current_cluster_std_data[ix_non_outlier]
+                best_labels_filtered=best_labels[ix_non_outlier]
+                nsub_clusters_filtered = len(np.unique(best_labels_filtered))
 
-                current_label=np.unique(possible_labels[current_cluster_indxs_filtered])[0]
+                current_unique_labels=np.unique(possible_labels[cluster.indxs])
+                current_label=current_unique_labels[0]
                 replacement_labels=get_replacement_current_cluster_labels(current_label, best_labels, next_cluster_label)
                 if(verbose):
                     print('perform_hierarchical_clustering: replace current cluster {}, label {}, by its subcluster labels'.format(clust_name, current_label), np.unique(replacement_labels))
@@ -341,7 +360,8 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
                 possible_labels_filtered=possible_labels[ix_good]
                 all_data_filtered=cluster_data[ix_good]
                 # compute score for this split on the original dataset minus the outliers
-                # should this be calculated on actual data or pca data?
+                # we have to calculate this on actual data as we are doing it for all data
+                # for which we do not have a normalised version
                 new_score=scorer.compute_score(all_data_filtered, possible_labels_filtered,verbose=verbose)
                 if(verbose):
                     print('perform_hierarchical_clustering: got the new {} score'.format(scorer.name), new_score)
@@ -358,25 +378,32 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
                 else:
                     if(verbose):
                         print('perform_hierarchical_clustering: level=',n_level," we accept this split and write all subclusters to the tree")
-                    # get the medoids of the clusters
-                    # calculate the centroids for the new clusters
+                    # Now we need to create the new subclusters.
+                    # First calculate the centroids from which we will get the medoids to be stored with each new subcluster
+                    # Get the medoids of the clusters based on normalised data
                     clf = NearestCentroid(metric='euclidean')
-                    print('perform_hierarchical_clustering: calculating centroids for current cluster with data:', current_cluster_pca_data_filtered.shape, best_labels[ix_non_outlier].shape, type(best_labels[0]))
-                    clf.fit(current_cluster_data_filtered, best_labels[ix_non_outlier])
-                    medoids_indx_filtered, distances_filtered =  pairwise_distances_argmin_min(clf.centroids_, current_cluster_data_filtered)
-                    medoids = current_cluster_data[medoids_indx_filtered,:]
-
-                    # now create nodes to add to the tree for the new sub clusters
-                    if(verbose):
+                    if verbose:
                         print('perform_hierarchical_clustering: creating {} subclusters'.format(nsub_clusters_filtered))
+                        print('perform_hierarchical_clustering: calculating centroids for current cluster with filtered std data')
+                    clf.fit(current_cluster_std_data_filtered, best_labels_filtered)
+                    # reverse transform the centroids to get centroids on original data
+                    orig_centroids=qt.inverse_transform(clf.centroids_)
+                    # There is a small chance that cluster1 has a C shape and cluster2 fits within the C which
+                    # would mean that the closest point to the centroid of cluster1 would actually be in cluster2.
+                    # Therefore we go round a loop to calculate medoids for each new subcluster just using the data
+                    # for that subcluster.
                     for c in range(nsub_clusters_filtered):
-                        ix=np.where(best_labels[ix_non_outlier]==c)[0]
-                        current_subcluster_indxs = current_cluster_indxs_filtered[ix]
-                        subcluster = ClusterNode("cl{}".format(c+1), cluster,current_subcluster_indxs, centroid=clf.centroids_[c],medoid=medoids[c])
+                        this_subcluster_data = current_cluster_data[best_labels == c,:]
+                        this_subcluster_std_data = current_cluster_std_data[best_labels == c,:]
+                        medoids_indx, distances_filtered =  pairwise_distances_argmin_min(clf.centroids_[c].reshape(1, -1), this_subcluster_std_data)
+                        this_medoids = this_subcluster_data[medoids_indx[0],:]
+                        # now create node to add to the tree for this new sub cluster
+                        ix=np.where(best_labels==c)[0]
+                        current_subcluster_indxs = cluster.indxs[ix]
+                        subcluster = ClusterNode("cl{}".format(c+1), cluster,cluster.indxs[best_labels==c], centroid=orig_centroids[c],medoid=this_medoids)
 
                     # now create any outlier node
                     if noutliers>0:
-                        outlier_data = current_cluster_data[ix_outlier]
                         if outlier_node==None:
                             if(verbose):
                                 print('perform_hierarchical_clustering: creating outlier node')
@@ -398,11 +425,8 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
                 print('perform_hierarchical_clustering: The list of scores looks like', scorer.scores)
         elif verbose:
             print('perform_hierarchical_clustering: level {}. We go to the next round'.format(n_level))
-        this_plot_fname="{}{}_tree_{}_loop.png".format(output_dir,plot_fname_prefix, n_level)
         if verbose:   
-            print('perform_hierarchical_clustering: plotting tree for loop', this_plot_fname)
-        root.plot_tree(this_plot_fname)
-        
+            root.print()
         out_fname='{}_{}_data_{}_level_{}_active_clusters.hdf'.format(plot_fname_prefix, root.name, str(n_level), str(next_cluster_label))
         if verbose:
             print('perform_hierarchical_clustering: writing tree data for level', n_level, out_fname)
@@ -411,7 +435,9 @@ def perform_hierarchical_clustering(cluster_data, field_list, evr_threshold, min
             clust_type='Kmeans'
         write_cluster_tree_hdf(root, clust_type, scorer_inner, scorer_outer, max_npoints_for_outlier, min_npoints_to_cluster, output_dir+out_fname)
 
-        n_level += 1
+        n_level += 1            
+    this_plot_fname=output_dir+plot_fname_prefix+'_tree.png'
+    root.plot_tree(this_plot_fname)
     scorer.plot_scores(output_dir+plot_fname_prefix+'_')
 
     print('perform_hierarchical_clustering: loop complete clustering is finished')

scores.py
@@ -16,12 +16,15 @@ from sklearn.neighbors import NearestCentroid
 #---------------------------------------------------------------------------------
 #    base class for handling scores - this must be inherited to use it but provides
 #    the definition for split_is_better and compute_score and does the plotting
+#    name just identifies the score
+#    min_max = 0 means a minimum score is best 1 means a maximum score is best
 #---------------------------------------------------------------------------------
 class Score(object):
-    def __init__(self):
+    def __init__(self, name, min_max):
         self.scores=[]
         self.nclusters=[]
-        self.name=''
+        self.name=name
+        self.min_max=min_max
 
     def add_score(self, new_score, this_nclusters=None):
         self.scores.append(new_score)
@@ -31,14 +34,22 @@ class Score(object):
     #---------------------------------------------------------------------------------
     #  split_is_better
     #    input:  new_score
-    #    return: True if the difference between new_score and last score in self.scores is positive or zero
-    #            (the last split makes the total clustering better)
-    #            False if the difference is negative (the last split doesn't help => the split before that was optimal)
+    #    return: True if new score is better, False otherwise. This is determined by whether 
+    #            we are looking for a minimum, maximum
     #---------------------------------------------------------------------------------
     def split_is_better(self, new_score):
+        better=False
         if len(self.scores)==0:
-            return True
-        return (new_score >= self.scores[-1]) 
+            # no previous scores so accept
+            better=True
+        else:
+            if self.min_max==1:
+                # looking for maximum score
+                better = new_score > self.scores[-1]
+            else:
+                # looking for minimum score
+                better = new_score < self.scores[-1]
+        return better
 
     #---------------------------------------------------------------------------------
     #    Computes score for a given clusters
@@ -64,6 +75,8 @@ class Score(object):
             x=np.arange(len(self.scores))+1
             xtitle='number of times'
         plt.scatter(x,y)
+        if len(self.scores)>1:
+            plt.plot(x,y)
         plt.title(self.name+' scores through the loops')
         plt.xlabel(xtitle)
         if len(x)>1:
@@ -74,11 +87,11 @@ class Score(object):
     
 #---------------------------------------------------------------------------------
 # class for handling Wemmert-Gancarski index
+# looking for a max score
 #---------------------------------------------------------------------------------
 class WG_Score(Score):
     def __init__(self):
-        super().__init__()
-        self.name='WG'
+        super().__init__('WG',1)
 
     #    Computes the Wemmert-Gancarski index for a given clusters
     def compute_score(self, X, labels, verbose=False):
@@ -115,14 +128,63 @@ class WG_Score(Score):
         
         WG = (1./N) * sum(np.multiply(np.array(J),n))
         return WG
+    #---------------------------------------------------------------------------------
+    #  split_is_better
+    #    input:  new_score
+    #    return: As for Score:split_is_better() but we also check here if the score is ==0 
+    #            because that should not be treated as better
+    #---------------------------------------------------------------------------------
+    def split_is_better(self, new_score):
+        if new_score==0:
+            return False
+        else:
+            return super().split_is_better(new_score)
     
 #---------------------------------------------------------------------------------
+# class for handling BIC score
+# looking for a max score
+#---------------------------------------------------------------------------------
+class BIC_Score(Score):
+    def __init__(self):
+        super().__init__('BIC',1)
+        
+    def compute_score(self, X, labels, verbose=False):
+        # get the centroids
+        clf = NearestCentroid(metric='euclidean')
+        clf.fit(X, labels)
+        #number of clusters
+        unique_lab,n=np.unique(labels, return_counts=True)
+        m = len(unique_lab)
+
+        #size of data set
+        N, d = X.shape
+
+        if(N != len(labels)):
+            if verbose:
+                print ("N is {} and d is {}".format(N,d))
+                print ("len(labels) is {}".format(len(labels)))
+            raise Exception('Cannot calculate BIC:The number of lines in the labels array is not the same as the number of lines in the data array!')
+            return (None)
+
+        #compute variance for all clusters beforehand
+        cl_var = (1.0 / (N - m) / d) * sum([sum(distance.cdist(X[np.where(labels == i)], [clf.centroids_[i][:]], 'sqeuclidean')) for i in range(m)])
+
+        const_term = 0.5 * m * np.log(N) * (d+1)
+
+        BIC = np.sum([n[i] * np.log(n[i]) -
+               n[i] * np.log(N) -
+             ((n[i] * d) / 2) * np.log(2*np.pi*cl_var) -
+             ((n[i] - 1) * d/ 2) for i in range(m)]) - const_term
+
+        return(BIC)
+
+#---------------------------------------------------------------------------------
 # class for handling calinski_harabaz score
+# we should look for the maximum score
 #---------------------------------------------------------------------------------
 class CalinskiHarabaz_Score(Score):
     def __init__(self):
-        super().__init__()
-        self.name='Calinski Harabaz'
+        super().__init__('Calinski Harabaz', 1)
 
     #    Computes the calinski harabaz score for given clusters
     def compute_score(self, X, labels, verbose=False):
@@ -130,35 +192,34 @@ class CalinskiHarabaz_Score(Score):
 
 #---------------------------------------------------------------------------------
 # class for handling silhouette score
+# we should look for the maximum score but not allow <=0
 #---------------------------------------------------------------------------------
 class Silhouette_Score(Score):
     def __init__(self):
-        super().__init__()
-        self.name='Silhouette'
+        super().__init__('Silhouette', 1)
 
     #    Computes the silhouette score for given clusters
     def compute_score(self, X, labels, verbose=False):
         return silhouette_score(X, labels)
+    #---------------------------------------------------------------------------------
+    #  split_is_better
+    #    input:  new_score
+    #    return: As for Score:split_is_better() but we also check here if the score is <=0 
+    #            because that should not be treated as better
+    #---------------------------------------------------------------------------------
+    def split_is_better(self, new_score):
+        if new_score<=0:
+            return False
+        else:
+            return super().split_is_better(new_score)
         
 #---------------------------------------------------------------------------------
 # class for handling davies_bouldin score
+# # we should look for the minimum score 
 #---------------------------------------------------------------------------------
 class DaviesBouldin_Score(Score):
     def __init__(self):
-        super().__init__()
-        self.name='Davies Bouldin'
-
-    #---------------------------------------------------------------------------------
-    #  split_is_better - this is the opposite for other scores, ie a lower score is better
-    #    input:  new_score
-    #    return: True if the difference between new_score and last score in self.scores is negative or zero
-    #            (the last split makes the total clustering better)
-    #            False if the difference is positive (the last split doesn't help => the split before that was optimal)
-    #---------------------------------------------------------------------------------
-    def split_is_better(self, new_score):
-        if len(self.scores)==0:
-            return True
-        return (new_score <= self.scores[-1])
+        super().__init__('Davies Bouldin', 0)
         
     #    Computes the davie bouldin score for given clusters
     def compute_score(self, X, labels, verbose=False):
@@ -174,6 +235,8 @@ def create_scorer(scorer_type):
         scorer=Silhouette_Score()
     elif scorer_type=='DB':
         scorer=DaviesBouldin_Score()
+    elif scorer_type=='BIC':
+        scorer=BIC_Score()
     else:
         raise Exception('Unknown scorer type'+scorer_type)
     return scorer

test_scores.py
@@ -1,56 +1,171 @@
 import pdb
 from scores import *
-from numpy import *
+import numpy as np
+import matplotlib.pyplot as plt
 
-scorer_wg=create_scorer('WG')
-scorer_ch=create_scorer('CH')
-scorer_sil=create_scorer('SIL')
-scorer_db=create_scorer('DB')
-scorers=[scorer_wg, scorer_ch, scorer_sil, scorer_db]
-nscorers=len(scorers)
+scorer_names=['WG','CH','SIL','DB','BIC']
+nscorers=len(scorer_names)
+scorers=[None]*nscorers
+for s in range(nscorers):
+    scorers[s]=create_scorer(scorer_names[s])
 scores=np.zeros(nscorers)
+# we will create nsamples data points with data split into 2 separated but close sets
+# we will check scores for completely random labels
+# we will then check scores for labels where a 3rd cluster has been made from the
+# points between the samples
+# we will then check scores for matching labels
 nsamples=1000
-nsamples_set1=300
-nvars=4
-# completely random data - should get low score
-print('test random data')
+nsamples_set0=300
+nsamples_set1=nsamples-nsamples_set0
+ix_set0=np.arange(nsamples_set0)
+ix_set1=np.arange(nsamples_set1)+nsamples_set0
+nvars=3
 X=np.zeros((nsamples,nvars))
-labels=np.asarray([np.random.randint(0,9) for i in range(nsamples)])
 for v in range(nvars):
-    X[:,v]=np.random.uniform(size=nsamples)
+    X[ix_set0,v]=np.random.uniform(size=nsamples_set0)
+    X[ix_set1,v]=np.random.uniform(size=nsamples_set1)+0.8
+nrows=3
+ncols=3
+size=2
+fig = plt.figure(figsize=(12,12*nrows/ncols))
+# completely random labels- should get low score
+#-----------------------------------------------
+labels=np.random.randint(0,2,size=nsamples)
+score_str=[]
+halfway_s=np.ceil(round(nscorers/2))
 for s in range(nscorers):
     scores[s]=scorers[s].compute_score(X,labels)
     scorers[s].add_score(scores[s])
-print('WG {}, CH {}, SIL {}, DB {}'.format(scores[0], scores[1], scores[2], scores[3]))
-assert(scores[2]<0) # silhouette score likley to be <0
-print('test separated data')
-labels[:nsamples_set1]=np.asarray([0 for i in range(nsamples_set1)])
-labels[nsamples_set1:]=np.asarray([1 for i in range(nsamples-nsamples_set1)])
-for v in range(nvars):
-    X[:nsamples_set1,v]=np.random.uniform(size=nsamples_set1)
-    X[nsamples_set1:,v]=np.random.uniform(size=nsamples-nsamples_set1)+5
+    if s==halfway_s:
+        score_str.append('\n'+scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
+    else:
+        score_str.append(scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
+score_str=', '.join(score_str)
+print('random label scores:')
+print('    '+'            '.join(scorer_names)+'\n', scores)
+#assert(scores[2]<=0.002) # silhouette score likley to be <=0
+# plot random data
+ix0=np.where(labels==0)[0]
+ix1=np.where(labels==1)[0]
+ax=fig.add_subplot(nrows,ncols,1,projection='3d')
+ax.set_title('random\n'+score_str, fontsize=10)
+ax.set_xlabel('x',fontsize=10)
+ax.set_ylabel('y', fontsize=10)
+ax.set_zlabel('z', fontsize=10)
+x=X[ix0,0]
+y=X[ix0,1]
+z=X[ix0,2]
+ax.scatter(x,y,z, marker='.', s=size, color='r')
+x=X[ix1,0]
+y=X[ix1,1]
+z=X[ix1,2]
+ax.scatter(x,y,z, marker='.', s=size, color='b')
+
+# partly matching labels
+#-----------------------------------------------
+good_labels=np.zeros(nsamples, int)
+good_labels[ix_set1]=1
+# switch some random labels around compared to good_labels
+part_labels=np.copy(good_labels)
+l0_labels=part_labels[ix_set0]
+l1_labels=part_labels[ix_set1]
+nto_shift_from0=int(nsamples_set0*0.1)
+nto_shift_from1=int(nsamples_set1*0.1)
+clf = NearestCentroid(metric='euclidean')
+clf.fit(X, good_labels)
+# for samples in cluster 0 find those closest to cluster 1 and vice versa
+dist1_to0=np.sum(distance.cdist(X[ix_set1,:], [clf.centroids_[0][:]], 'sqeuclidean'),axis=1)
+dist0_to1=np.sum(distance.cdist(X[ix_set0,:], [clf.centroids_[1][:]], 'sqeuclidean'),axis=1)
+ix=np.argsort(dist1_to0)
+l1_labels[ix[:nto_shift_from1]]=2
+ix=np.argsort(dist0_to1)
+l0_labels[ix[:nto_shift_from0]]=2
+part_labels[ix_set0]=l0_labels
+part_labels[ix_set1]=l1_labels
+score_str=[]
 for s in range(nscorers):
-    scores[s]=scorers[s].compute_score(X,labels)
+    scores[s]=scorers[s].compute_score(X,part_labels)
+    if s==halfway_s:
+        score_str.append('\n'+scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
+    else:
+        score_str.append(scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
     assert(scorers[s].split_is_better(scores[s]))
     scorers[s].add_score(scores[s])
-print('WG {}, CH {}, SIL {}, DB {}'.format(scores[0], scores[1], scores[2], scores[3]))
-print('test partly separated data')
-for v in range(nvars):
-    X[:nsamples_set1,v]=np.random.uniform(size=nsamples_set1)
-    X[nsamples_set1:,v]=np.random.uniform(size=nsamples-nsamples_set1)+0.5
+score_str=', '.join(score_str)
+print('partly good labels scores:')
+print('    '+'            '.join(scorer_names)+'\n', scores)
+# plot partly good labels
+ix0=np.where(part_labels==0)[0]
+ix1=np.where(part_labels==1)[0]
+ix2=np.where(part_labels==2)[0]
+ax=fig.add_subplot(nrows,ncols,2,projection='3d')
+ax.set_title('part\n'+score_str, fontsize=10)
+ax.set_xlabel('x',fontsize=10)
+ax.set_ylabel('y', fontsize=10)
+ax.set_zlabel('z', fontsize=10)
+x=X[ix0,0]
+y=X[ix0,1]
+z=X[ix0,2]
+ax.scatter(x,y,z, marker='.', s=size, color='r')
+x=X[ix1,0]
+y=X[ix1,1]
+z=X[ix1,2]
+ax.scatter(x,y,z, marker='.', s=size, color='b')
+x=X[ix2,0]
+y=X[ix2,1]
+z=X[ix2,2]
+ax.scatter(x,y,z, marker='.', s=size, color='g')
+
+# good labels
+#-----------------------------------------------
+score_str=[]
 for s in range(nscorers):
-    scores[s]=scorers[s].compute_score(X,labels)
-    assert(scorers[s].split_is_better(scores[s])==False)
-print('WG {}, CH {}, SIL {}, DB {}'.format(scores[0], scores[1], scores[2], scores[3]))
+    scores[s]=scorers[s].compute_score(X,good_labels)
+    if s==halfway_s:
+        score_str.append('\n'+scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
+    else:
+        score_str.append(scorer_names[s]+'={sc:.2f} '.format(sc=scores[s]))
+    assert(scorers[s].split_is_better(scores[s]))
+    scorers[s].add_score(scores[s])
+score_str=', '.join(score_str)
+print('good label scores:')
+print('    '+'            '.join(scorer_names)+'\n', scores)
+# plot good labels
+ax=fig.add_subplot(nrows,ncols,3,projection='3d')
+ax.set_title('good labels\n'+score_str, fontsize=10)
+ax.set_xlabel('x',fontsize=10)
+ax.set_ylabel('y', fontsize=10)
+ax.set_zlabel('z', fontsize=10)
+x=X[ix_set0,0]
+y=X[ix_set0,1]
+z=X[ix_set0,2]
+ax.scatter(x,y,z, marker='.', s=size, color='r')
+x=X[ix_set1,0]
+y=X[ix_set1,1]
+z=X[ix_set1,2]
+ax.scatter(x,y,z, marker='.', s=size, color='b')
+
+xticks=np.arange(3)
+for s in range(nscorers):
+    ax=fig.add_subplot(nrows,ncols,4+s)
+    ax.scatter(xticks,scorers[s].scores, marker='*')
+    ax.set_xticks(xticks, labels=['random','part', 'good'])
+    plt.title(scorer_names[s])
+
+plt.gcf().subplots_adjust(wspace=0.3, hspace=0.4)
+plt.show()
+plt.close()
+
 # add some scores and check they plot properly
 scorer_wg=create_scorer('WG')
 scorer_wg.add_score(0.2,2)
 scorer_wg.add_score(0.3,3)
 scorer_wg.add_score(0.25,4)
-print('scores',scorer_wg.scores)
+print('check score plotting')
+print('WG scores',scorer_wg.scores)
 print('nclusters',scorer_wg.nclusters)
 #do the same but with just one score
-scorer_wg.plot_scores('./')
+scorer_wg.plot_scores(./testing/')
 scorer_wg=create_scorer('WG')
 scorer_wg.add_score(0.3,3)
-scorer_wg.plot_scores('./second_')
+scorer_wg.plot_scores('./testing/second_')